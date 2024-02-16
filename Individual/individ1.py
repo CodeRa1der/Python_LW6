d22e4f0925959bee4c4bcae84f99ecfbb90162b0
Code_Ra1der <kostydu2342@gmail.com>
2024-02-16 20:25:26 +0300

    Minor fixes

--- Individual/individ1.py
+++ Individual/individ1.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-sentence = input("Введите преддложение: ")
-count = 1
+if __name__ == '__main__':
+    sentence = input("Введите преддложение: ")
+    count = 1
 
-for char in sentence:
-    if count % 3 == 0:
-        print(char)
-    count += 1
+    for char in sentence:
+        if count % 3 == 0:
+            print(char)
+        count += 1
--- Individual/individ3.py
+++ Individual/individ3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-word = input("Введите слово с точкой в конце: ")
-replacer = 'a'
-for i in range(len(word)):
-    if i == 5:
-        print('Новое слово:', word[:5] + replacer + word[5:])
+if __name__ == '__main__':
+    word = input("Введите слово с точкой в конце: ")
+    replacer = 'a'
+    for i in range(len(word)):
+        if i == 5:
+            print('Новое слово:', word[:5] + replacer + word[5:])
